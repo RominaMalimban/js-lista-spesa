7714ec3fffd0ba110cdd22154304d1a963c5836d
RominaMalimban <romina.lyn@hotmail.it>
2022-11-14 15:04:46 +0100

    ciclo for eseguito

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
+
+// 1) CICLO FOR: 
+
+// let spesa = ["pasta", "riso", "pollo", "pesce", "latte"];
+// let lista = document.querySelector(".lista");
+
+// for (let i = 0; i < spesa.length; i++){
+
+//     let item = `<div>${spesa[i]}</div>`;
+//     lista.innerHTML += item;
+// }
+
